3464cc4c30e0d571396a4176c4cdc60aa095ec18
AndriiPetrechko <petrechkoandrij@gmail.com>
2020-10-10 21:15:56 +0300

    page4 fix animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
 
 gsap.from("#p4_img1", {
+    scrollTrigger: {
+        trigger: "#p4_img1",
+        toggleActions: "restart"
+    },
     opacity: 0, 
     x: -300, 
     duration: 2
-  });
+});
+
+gsap.from("#p4_img2", {
+    scrollTrigger: {
+        trigger: "#p4_img2",
+        toggleActions: "restart"
+    },
+    opacity: 0, 
+    x: 300, 
+    duration: 1.5
+});
