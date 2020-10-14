514dd992df1ebc8c446237eaa20db59c30960a19
AndriiPetrechko <petrechkoandrij@gmail.com>
2020-10-14 08:56:29 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ gsap.from("#p4_img2", {
     x: 300, 
     duration: 1.5
 });
+
+gsap.from("#onion_img", {
+    opacity: 0, 
+    rotation: 270, 
+    duration: 2.5
+});
+gsap.from("#leaf", {
+    opacity: 0, 
+    rotation: -180, 
+    duration: 2.5
+});
+
+
